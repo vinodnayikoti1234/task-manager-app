35bc04b4f59cf1b792a36575509e57722adecb9c
vinodnayikoti1234 <nayikotivinod@gmail.com>
2025-10-26 11:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/data/users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
